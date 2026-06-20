f65256aa0f0e14bcad019b6122f187fbdfdaf911
Aneesh Thakur <aneeshthakur134@gmail.com>
2026-06-20 09:12:08 +0530

    Add patient details and doctor relationship to Reception

--- hospitalmanagement/entity/Reception.java
+++ hospitalmanagement/entity/Reception.java
@@ -5,4 +5,14 @@ package com.stickytechnologies.hospitalmanagement.entity;
 public class Reception{
   @Id
   private String patient_id;
+
+  private String patient_name;
+  private String patient_address;
+  private String patient_gender;
+  private String patient_mobileno;
+  private String patient_disease;
+
+  @OneToMany(mappedBy="patient")
+  @JsonManagedReference
+  private List<Doctors> doctor;
 }
